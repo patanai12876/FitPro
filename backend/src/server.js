2853968c93907b6c552690183e7ceda539e3cff6
Noor <noorahsaan3@gmail.com>
2026-06-01 01:22:08 +0500

    backend ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ connectDB();
 // Security Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN ,
   credentials: true
 }));
 
